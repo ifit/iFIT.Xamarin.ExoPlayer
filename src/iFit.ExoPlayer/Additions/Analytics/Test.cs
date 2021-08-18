e6a3d0333e7426a27ed9de950af2e45e33fe6282
Kara <kara.c.swindler@gmail.com>
2021-08-18 10:14:03 -0700

    Testing

--- src/iFit.ExoPlayer/Additions/Analytics/Test.cs
+++ src/iFit.ExoPlayer/Additions/Analytics/Test.cs
@@ -1,1 +1,24 @@
-// Testing
+using System.Linq;
+
+// NOTE No way to override base methods with new types in C#
+
+namespace Google.Android.ExoPlayer2.Analytics
+{
+    public partial class AnalyticsListenerEvents
+    {
+        public bool Contains (AnalyticsEventFlags eventFlag)
+        {
+            return base.Contains ((int) eventFlag);
+        }
+
+        public bool ContainsAny (params AnalyticsEventFlags [] eventFlags)
+        {
+            return base.ContainsAny (eventFlags.Cast<int> ().ToArray ());
+        }
+
+        public new AnalyticsEventFlags Get (int index)
+        {
+            return (AnalyticsEventFlags) base.Get (index);
+        }
+    }
+}
